18ebe3c9fe127db824a7a61e41a0207a0dd9bd3e
daiglerj <daiglerj@canisius.edu>
2018-03-08 22:45:50 -0500

    Formatted section to search a course on the homepage

--- src/Components/CourseSearch.js
+++ src/Components/CourseSearch.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
 import './../App.css';
 import TextField from 'material-ui/TextField';
-
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 export default class CourseSearch extends Component {
     constructor(props){
         super(props)
     }
     
     render(){
-        var inputStyle = {
-            width: "600px"
+        var formStyle = {
+            border:"1px solid lightgray",
+            borderRadius: "5px",
+            width: "600px",
+            margin:"auto",
+            boxShadow: "-2px 1px #c7c7c7",
+            padding: "10px"
+        }
+        var dropdownBox = {
+            borderRight:"solid #c7c7c7 2px",
+            paddingRight: "30px",
+            marginRight: "30px",
+            display:"inline-block",
+            height: "50px",
+            lineHeight:"50px"
+        }
+        var searchStyle = {
+            padding: "10px",
+            width: "300px",
+            border:"none",
+            fontSize: "15px"
+        }
+        var selectStyle = {
+            width: "200px",
+            height:"30px",
+            border:"none",
+            fontSize: "15px"
         }
         return(
             <div className = "courseSearch">
-                    <TextField
-                        hintText="Find Lesson Plans and Course Material"
-                        multiLine={true}
-                        rows={2}
-                        inputStyle={inputStyle}
-                        underlineShow={false}
-                    />
+                <div style={formStyle}>
+                    <span style = {dropdownBox}>
+                        <select className= "selectCategory" style = {selectStyle}>
+                            <option value="" disabled selected>Select education level</option>
+
+                            <option>Freshman</option>
+                            <option>Underclassman</option>
+                            <option>Upperclassman</option>
+                            <option>Graduate</option>
+
+                        </select>
+                    </span>
+                    <input className="searchCourse" style={searchStyle} placeholder="Find Lesson Plans and Course Material" />
+                </div>
             </div>
         )
     }
